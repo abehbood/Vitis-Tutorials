109a3023616a3dd39fa04183b968e65902fdbba9
viswanad <viswanad@xhdviswanad41x.xlnx.xilinx.com>
2021-01-25 16:43:46 +0530

    Added init.m file

---
+++ AI_Engine_Development/Design_Tutorials/02-super_sampling_rate_fir/ModelComposer/MultiKernel/init.m
@@ -0,0 +1,32 @@
+DISPLAY = 0;
+
+Norm = 32768;
+i = sqrt(-1);
+UTaps = CreateFilter(32);
+Taps = NormalizeCoef(UTaps,32768);
+
+if(DISPLAY==1)
+fprintf('---------------------------------------------------------------------\n');
+fprintf('Normalization Coef %8d --> Result %d\n\n',Norm,sum(IntCoef));
+fprintf('Real Part     : %8d  ->  %8d\n',min(real(IntCoef)),max(real(IntCoef)));
+fprintf('Imaginary Part: %8d  ->  %8d\n',min(imag(IntCoef)),max(imag(IntCoef)));
+fprintf('---------------------------------------------------------------------\n\n\n');
+Lspec = 2048;
+freqz(Taps/(Norm/Lspec),Lspec,'whole');
+end
+
+SaveTaps(Taps,'src/FilterTaps.txt');
+
+
+SHIFT_ACC = 15;
+BURST_LENGTH = 512;
+
+Taps_1 = Taps(1:8);
+Taps_2 = Taps(9:16);
+Taps_3 = Taps(17:24);
+Taps_4 = Taps(25:32);
+
+% phi_3 = fliplr(Taps(1:4:end));
+% phi_2 = fliplr(Taps(2:4:end));
+% phi_1 = fliplr(Taps(3:4:end));
+% phi_0 = fliplr(Taps(4:4:end));
